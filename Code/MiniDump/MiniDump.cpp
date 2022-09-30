c2fa831bbd9680b8219f7d8a667ab7498e988c88
weishengzu <shengzu.wei@hiscene.com>
2022-09-30 17:57:17 +0800

    update

--- Code/MiniDump/MiniDump.cpp
+++ Code/MiniDump/MiniDump.cpp
@@ -1,61 +1,77 @@
 #include "MiniDump.h"
 
+namespace fs = std::filesystem;
 
-MiniDump::MiniDump()
-{
+MiniDump::MiniDump() {}
+
+MiniDump::~MiniDump() {}
+
+void MiniDump::EnableAutoDump(bool bEnable) {
+  if (bEnable) {
+    SetUnhandledExceptionFilter(
+        (LPTOP_LEVEL_EXCEPTION_FILTER)ApplicationCrashHandler);
+  }
 }
 
+LONG MiniDump::ApplicationCrashHandler(EXCEPTION_POINTERS *pException) {
+  /*if (IsDebuggerPresent())
+  {
+          return EXCEPTION_CONTINUE_SEARCH;
+  }*/
 
-MiniDump::~MiniDump()
-{
+  TCHAR szDumpFile[MAX_PATH] = {0};
+  TCHAR szMsg[MAX_PATH] = {0};
+  SYSTEMTIME stTime = {0};
+  auto dumpPath = GetminiDumpDir().c_str();
+  auto nextMinidumpId = GetCurrentDateTime().c_str();
+  // 构建dump文件路径;
+  TSprintf(szDumpFile, "%s\\%s.dmp", dumpPath, nextMinidumpId);
+  // 创建dump文件;
+  CreateDumpFile(szDumpFile, pException);
+
+  // 弹出一个错误对话框或者提示上传， 并退出程序;
+  TSprintf(szMsg,
+           _T("I'm so sorry, but the program crashed.\r\ndump file : %s"),
+           szDumpFile);
+  FatalAppExit(-1, szMsg);
+
+  return EXCEPTION_EXECUTE_HANDLER;
 }
 
-void MiniDump::EnableAutoDump(bool bEnable)
-{
-	if (bEnable)
-	{
-		SetUnhandledExceptionFilter((LPTOP_LEVEL_EXCEPTION_FILTER) ApplicationCrashHandler);
-	}
+void MiniDump::CreateDumpFile(LPCSTR strPath, EXCEPTION_POINTERS *pException) {
+  // 创建Dump文件;
+  HANDLE hDumpFile = CreateFile(strPath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
+                                FILE_ATTRIBUTE_NORMAL, NULL);
+  // Dump信息;
+  MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
+  dumpInfo.ExceptionPointers = pException;
+  dumpInfo.ThreadId = GetCurrentThreadId();
+  dumpInfo.ClientPointers = TRUE;
+
+  // 写入Dump文件内容;
+  MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hDumpFile,
+                    MiniDumpNormal, &dumpInfo, NULL, NULL);
+  CloseHandle(hDumpFile);
 }
 
-LONG MiniDump::ApplicationCrashHandler(EXCEPTION_POINTERS *pException)
-{
-	/*if (IsDebuggerPresent())
-	{
-		return EXCEPTION_CONTINUE_SEARCH;
-	}*/
-
-	TCHAR szDumpDir[MAX_PATH] = { 0 };
-	TCHAR szDumpFile[MAX_PATH] = { 0 };
-	TCHAR szMsg[MAX_PATH] = { 0 };
-	SYSTEMTIME	stTime = { 0 };
-	// 构建dump文件路径;
-	GetLocalTime(&stTime);
-	::GetCurrentDirectory(MAX_PATH, szDumpDir);
-	TSprintf(szDumpFile, _T("%s\\%04d%02d%02d_%02d%02d%02d.dmp"), szDumpDir,
-		stTime.wYear, stTime.wMonth, stTime.wDay,
-		stTime.wHour, stTime.wMinute, stTime.wSecond);
-	// 创建dump文件;
-	CreateDumpFile(szDumpFile, pException);
-
-	// 弹出一个错误对话框或者提示上传， 并退出程序;
-	TSprintf(szMsg, _T("I'm so sorry, but the program crashed.\r\ndump file : %s"), szDumpFile);
-	FatalAppExit(-1, szMsg);
-
-	return EXCEPTION_EXECUTE_HANDLER;
+std::string MiniDump::GetCurrentDateTime() {
+  std::time_t timep;
+  std::time(&timep);
+  char tmp[64];
+  strftime(tmp, sizeof(tmp), "%Y-%m-%d %H-%M-%S", std::localtime(&timep));
+  return tmp;
 }
 
-void MiniDump::CreateDumpFile(LPCWSTR strPath, EXCEPTION_POINTERS *pException)
-{
-	// 创建Dump文件;
-	HANDLE hDumpFile = CreateFile(strPath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	// Dump信息;
-	MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
-	dumpInfo.ExceptionPointers = pException;
-	dumpInfo.ThreadId = GetCurrentThreadId();
-	dumpInfo.ClientPointers = TRUE;
-
-	// 写入Dump文件内容;
-	MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hDumpFile, MiniDumpNormal, &dumpInfo, NULL, NULL);
-	CloseHandle(hDumpFile);
+std::string MiniDump::GetminiDumpDir() {
+  std::string dump_dir = fs::current_path().string().append("/minidump");
+  fs::path dump_path_dir(dump_dir);
+  try {
+    if (!fs::exists(dump_path_dir)) {
+      fs::create_directories(dump_path_dir);
+    }
+  } catch (const fs::filesystem_error &ex) {
+    std::cout << "path create failed: " << ex.what()
+              << " path is: " << dump_path_dir << std::endl;
+  }
+  return dump_dir;
 }
--- Code/MiniDump/MiniDump.h
+++ Code/MiniDump/MiniDump.h
@@ -1,30 +1,47 @@
+/*
+ * @Author: shengzu@wei
+ * @Date: 2022-09-30 16:17:22
+ * @LastEditTime: 2022-09-30 17:56:28
+ * @LastEditors: shengzu@wei
+ * @Description:
+ */
 #pragma once
-#include <Windows.h>
+
+#include <stdio.h>
 #include <tchar.h>
-#include <DbgHelp.h>
+#include <time.h>
+#include <windows.h>
+
+#include <ctime>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <dbghelp.h>
 #pragma comment(lib, "dbghelp.lib")
 
 #ifdef UNICODE
-#define TSprintf	wsprintf
+#define TSprintf wsprintf
 #else
-#define TSprintf	sprintf
+#define TSprintf sprintf
 #endif
 
+class MiniDump {
+ private:
+  MiniDump();
+  ~MiniDump();
 
-class MiniDump
-{
-private:
-	MiniDump();
-	~MiniDump();
+ public:
+  // 程序崩溃时是否启动自动生成dump文件;
+  // 只需要在main函数开始处调用该函数即可;
+  static void EnableAutoDump(bool bEnable = true);
 
-public:
-	// 程序崩溃时是否启动自动生成dump文件;
-	// 只需要在main函数开始处调用该函数即可;
-	static void EnableAutoDump(bool bEnable = true);
+ private:
+  static LONG ApplicationCrashHandler(EXCEPTION_POINTERS *pException);
 
-private:
+  static void CreateDumpFile(LPCSTR lpstrDumpFilePathName,
+                             EXCEPTION_POINTERS *pException);
 
-	static LONG ApplicationCrashHandler(EXCEPTION_POINTERS *pException);
+  static std::string GetCurrentDateTime();
 
-	static void CreateDumpFile(LPCWSTR lpstrDumpFilePathName, EXCEPTION_POINTERS *pException);
+  static std::string GetminiDumpDir();
 };
